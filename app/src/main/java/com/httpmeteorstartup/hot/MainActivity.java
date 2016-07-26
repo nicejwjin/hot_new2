d9aa77f124be554ff40c523a002cc6c833648b5b
jwjin <jwjin@me.com>
2016-07-27 02:08:20 +0900

    init

---
+++ app/src/main/java/com/httpmeteorstartup/hot/MainActivity.java
@@ -0,0 +1,79 @@
+package com.httpmeteorstartup.hot;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    TextView text1, text2;
+    CheckBox chkAgree;
+    RadioGroup rGroup1;
+    RadioButton rdoDog, rdoCat, rdoRabbit;
+    Button btnOK;
+    ImageView imgPet;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("show animals");
+
+        text1 = (TextView) findViewById(R.id.Text1);
+        chkAgree = (CheckBox) findViewById(R.id.ChkAgree);
+
+        text2 = (TextView) findViewById(R.id.Text2);
+        rGroup1 = (RadioGroup) findViewById(R.id.Rgroup1);
+        rdoDog = (RadioButton) findViewById(R.id.RdoDog);
+        rdoCat = (RadioButton) findViewById(R.id.RdoCat);
+        rdoRabbit = (RadioButton) findViewById(R.id.RdoRabbit);
+
+        btnOK = (Button) findViewById(R.id.BtnOK);
+        imgPet = (ImageView) findViewById(R.id.ImgPet);
+
+        chkAgree.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
+                if (chkAgree.isChecked() == true) {
+                    text2.setVisibility(android.view.View.VISIBLE);
+                    rGroup1.setVisibility(android.view.View.VISIBLE);
+                    btnOK.setVisibility(android.view.View.VISIBLE);
+                    imgPet.setVisibility(android.view.View.VISIBLE);
+                } else {
+                    text2.setVisibility(android.view.View.INVISIBLE);
+                    rGroup1.setVisibility(android.view.View.INVISIBLE);
+                    btnOK.setVisibility(android.view.View.INVISIBLE);
+                    imgPet.setVisibility(android.view.View.INVISIBLE);
+                }
+            }
+        });
+
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+
+                switch (rGroup1.getCheckedRadioButtonId()) {
+                    case R.id.RdoDog:
+                        imgPet.setImageResource(R.drawable.imgres_3);
+                        break;
+                    case R.id.RdoCat:
+                        imgPet.setImageResource(R.drawable.imgres_1);
+                        break;
+                    case R.id.RdoRabbit:
+                        imgPet.setImageResource(R.drawable.imgres_2);
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Select animals first", Toast.LENGTH_SHORT)
+                                .show();
+                }
+            }
+        });
+
+    }
+
+}
